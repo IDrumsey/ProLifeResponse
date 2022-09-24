39b857acca51421024c248084fcbcb55d110e4c7
IDrumsey <clayofcasper@gmail.com>
2022-09-23 21:58:26 -0400

    Added mantine.

--- proliferesponses/pages/_app.tsx
+++ proliferesponses/pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+    <>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light'
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
   return <Component {...pageProps} />
 }
 
